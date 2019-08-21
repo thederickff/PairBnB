3475e1f4732318f98ccafab213402877975c9fdf
Derick Felix <derickfelix4321@gmail.com>
2019-08-21 08:24:55 -0400

    Setup forward and backward navigation

--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-place-detail',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlaceDetailPage implements OnInit {
 
-  constructor() { }
+  constructor(private navCtrl: NavController) { }
 
   ngOnInit() {
+  }
+
+  bookPlace() {
+    this.navCtrl.navigateBack('/places/tabs/discover');
   }
 
 }
