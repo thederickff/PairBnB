fefd3053a53df5c120185a9a114a38e97d07bd3b
Derick Felix <derickfelix4321@gmail.com>
2019-08-21 07:38:17 -0400

    Add tabs to the app

--- src/app/places/places.module.ts
+++ src/app/places/places.module.ts
@@ -1,26 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { Routes, RouterModule } from '@angular/router';
-
 import { IonicModule } from '@ionic/angular';
 
 import { PlacesPage } from './places.page';
-
-const routes: Routes = [
-  {
-    path: '',
-    component: PlacesPage
-  }
-];
+import { PlacesRoutingModule } from './places-routing.module';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    RouterModule.forChild(routes)
-  ],
+  imports: [CommonModule, IonicModule, PlacesRoutingModule],
   declarations: [PlacesPage]
 })
 export class PlacesPageModule {}
