4e07bd1950f0c20975e70b51b7de0bfd5b3dd8d1
Derick Felix <derickfelix4321@gmail.com>
2019-08-27 07:34:58 -0400

    Finish grid

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AuthService {
-  private mUserIsAuthenticated = false;
+  private mUserIsAuthenticated = true;
 
   get userIsAuthenticated() {
     return this.mUserIsAuthenticated;
