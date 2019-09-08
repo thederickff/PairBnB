f336ffcea01cdbb17fedcadb629a971aa5fdb572
Derick Felix <derickfelix4321@gmail.com>
2019-09-08 12:44:36 -0400

    Add color and styling to the app

---
+++ src/app/places/offers/offer-item/offer-item.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Place } from '../../place.model';
+
+@Component({
+  selector: 'app-offer-item',
+  templateUrl: './offer-item.component.html',
+  styleUrls: ['./offer-item.component.scss'],
+})
+export class OfferItemComponent implements OnInit {
+
+  @Input() place: Place;
+
+  constructor() { }
+
+  ngOnInit() {}
+
+  getDummyDate() {
+    return new Date();
+  }
+}
