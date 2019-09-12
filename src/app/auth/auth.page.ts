0bd663ffdccef509522fda89807af9351e3cf195
Derick Felix <derickfelix4321@gmail.com>
2019-09-12 19:49:29 -0400

    Handle input validation

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
 import { LoadingController } from '@ionic/angular';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -29,5 +30,9 @@ export class AuthPage implements OnInit {
       loading.dismiss();
       this.router.navigateByUrl('/places/tabs/discover');
     }, 1500);
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form);
   }
 }
