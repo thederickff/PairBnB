d7da0dc0d744929c84f792fdc6a096285896dcf0
Derick Felix <derickfelix4321@gmail.com>
2019-09-14 18:37:06 -0400

    Add create-booking form

--- src/app/bookings/create-booking/create-booking.component.ts
+++ src/app/bookings/create-booking/create-booking.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { Place } from '../../places/place.model';
 import { ModalController } from '@ionic/angular';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-create-booking',
@@ -9,16 +10,56 @@ import { ModalController } from '@ionic/angular';
 })
 export class CreateBookingComponent implements OnInit {
   @Input() selectedPlace: Place;
+  @Input() selectedMode: 'select' | 'random';
+  @ViewChild('f', null) form: NgForm;
+  startDate: string;
+  endDate: string;
 
   constructor(private modalCtrl: ModalController) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    const availableFrom = new Date(this.selectedPlace.availableFrom);
+    const availableTo = new Date(this.selectedPlace.availableTo);
+
+    if (this.selectedMode === 'random') {
+      this.startDate = new Date(
+        availableFrom.getTime() +
+          Math.random() *
+            (availableTo.getTime() -
+              7 * 24 * 60 * 60 * 1000 -
+              availableFrom.getTime())
+      ).toISOString();
+
+      const startTime = new Date(this.startDate).getTime();
+      this.endDate = new Date(
+        startTime +
+          Math.random() * (startTime + 6 * 24 * 60 * 60 * 1000 - startTime)
+      ).toISOString();
+    }
+  }
 
   onBookPlace() {
-    this.modalCtrl.dismiss({ message: 'This is a dummy message' }, 'confirm');
+    if (this.form.invalid || !this.datesValid) {
+      return;
+    }
+
+    this.modalCtrl.dismiss({ bookingDate: {
+      firstName: this.form.value['first-name'],
+      lastName: this.form.value['last-name'],
+      guestNumber: this.form.value['guest-number'],
+      startDate: this.form.value['date-from'],
+      endDate: this.form.value['date-to']
+    } }, 'confirm');
   }
 
   onCancel() {
     this.modalCtrl.dismiss(null, 'cancel');
+  }
+
+  datesValid() {
+    const startDate = new Date(this.form.value['date-from']);
+    const endDate = new Date(this.form.value['date-to']);
+
+    return endDate > startDate;
   }
 }
--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -11,21 +11,27 @@ export class PlacesService {
       'Manhattan Mansion',
       'In the heart of New York Ciry',
       'https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fgzbrokers.files.wordpress.com%2F2012%2F02%2Fnewyorkcity-realestate-night.jpg&f=1',
-      149.99
+      149.99,
+      new Date('2019-09-14'),
+      new Date('2019-09-20')
     ),
     new Place(
       '2',
       'L\'Amour Toujours',
       'A romantic place in Paris!',
       'https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fs-ec.bstatic.com%2Fimages%2Fhotel%2Fmax1024x768%2F696%2F69653904.jpg&f=1',
-      189.99
+      189.99,
+      new Date('2019-09-16'),
+      new Date('2019-09-22')
     ),
     new Place(
       '3',
       'The DK Osten Ved',
       'Not your average city trip!',
       'https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fjohnrieber.files.wordpress.com%2F2014%2F01%2Fcopenhagen-harbor-denmark.jpg&f=1',
-      119.99
+      119.99,
+      new Date('2019-09-15'),
+      new Date('2019-09-20')
     )
   ];
 
@@ -36,7 +42,9 @@ export class PlacesService {
       '60 smoke-free guestrooms, Restaurant and 2 restaurants and Rooftop terrace',
       'https://thumbnails.trvl-media.com/_1zvG_S5crNfs1YLEIF1pDZLgLw=/582x388/smart/' +
         'filters:quality(60)/exp.cdn-hotels.com/hotels/1000000/30000/21400/21358/ca08b544_z.jpg',
-      179.99
+      179.99,
+      new Date('2019-09-18'),
+      new Date('2019-09-22')
     ),
     new Place(
       '5',
@@ -44,7 +52,9 @@ export class PlacesService {
       'Luxury hotel with 3 restaurants, near Teatro Metropólitan',
       'https://thumbnails.trvl-media.com/0ydepJNDssbrZpnEfbuIfdAuB-4=/582x388/smart/' +
         'filters:quality(60)/exp.cdn-hotels.com/hotels/1000000/30000/25800/25798/58952a54_z.jpg',
-      95.99
+      95.99,
+      new Date('2019-09-19'),
+      new Date('2019-09-29')
     ),
     new Place(
       '6',
@@ -52,7 +62,9 @@ export class PlacesService {
       '4-star hotel with restaurant, near Plaza de la Constitucion.',
       'https://thumbnails.trvl-media.com/ckH8tk4raeUF8mUKE9P7jCIfUMs=/582x388/smart/' +
         'filters:quality(60)/exp.cdn-hotels.com/hotels/2000000/1360000/1354000/1353939/d8d86787_z.jpg',
-      107.99
+      107.99,
+      new Date('2019-10-01'),
+      new Date('2019-10-14')
     )
   ];
 
