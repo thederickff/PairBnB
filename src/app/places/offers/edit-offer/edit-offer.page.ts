19bc7cb619c67bea13869ee3392106a1c1be67d8
Derick Felix <derickfelix4321@gmail.com>
2019-09-14 14:31:32 -0400

    Implement edit-offer form

--- src/app/places/offers/edit-offer/edit-offer.page.ts
+++ src/app/places/offers/edit-offer/edit-offer.page.ts
@@ -3,6 +3,7 @@ import { Place } from '../../place.model';
 import { ActivatedRoute } from '@angular/router';
 import { NavController } from '@ionic/angular';
 import { PlacesService } from '../../places.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-offer',
@@ -12,6 +13,7 @@ import { PlacesService } from '../../places.service';
 export class EditOfferPage implements OnInit {
 
   place: Place;
+  form: FormGroup;
 
   constructor(
     private router: ActivatedRoute,
@@ -27,6 +29,25 @@ export class EditOfferPage implements OnInit {
       }
 
       this.place = this.placesService.getPlace2(paramMap.get('placeId'));
+
+      this.form = new FormGroup({
+        title: new FormControl(this.place.title, {
+          updateOn: 'blur',
+          validators: [Validators.required]
+        }),
+        description: new FormControl(this.place.description, {
+          updateOn: 'blur',
+          validators: [Validators.required, Validators.maxLength(180)]
+        })
+      });
     });
+  }
+
+  onUpdateOffer() {
+    if (this.form.invalid) {
+      return;
+    }
+
+    console.log(this.form);
   }
 }
